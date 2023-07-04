da59e7c410280bf764a977ae5f42a635f988bc74
Eunji Hwang <53595582+carminchameleon@users.noreply.github.com>
2023-07-04 22:24:57 +1000

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Eunji Hwang on 2023/07/04.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
